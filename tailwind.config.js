5b217e1ced4ff4c951b7820f2b763f496c71e20d
sondmfpt <sondmhe186291@fpt.edu.vn>
2024-09-12 23:22:59 +0700

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./build/web/**/*.{html,js,jsp}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'coffee': {
+          200: '#eee1c4',
+          500: '#dcb386',
+          700: '#9e826c' //hover
+        }
+      },
+      fontFamily: {
+        'serif': "'Abril Fatface', serif"
+      }
+    },
   },
   plugins: [],
 }
